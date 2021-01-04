30eb0eb9bc77326dc82b1537b23eaebd640debaa
Cokorda Raka Angga Jananuraga <raka.moviz@gmail.com>
2021-01-04 16:42:24 -0600

    initial}

---
+++ go.mod
@@ -0,0 +1,3 @@
+module raka.com/go-structures/basic
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+
+  "raka.com/go-structures/basic/morestrings"
+)
+
+func main() {
+  fmt.Println(morestrings.ReverseRunes("Hello, Go!"))
+}
